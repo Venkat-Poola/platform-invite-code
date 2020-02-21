ea5f576d87f01094c6f2ca3ea98506df1d4d4726
Srinivasa Peddibhotla <srinivasa.peddibhotl@Administrators-MacBook-Pro.local>
2020-02-20 16:43:36 -0800

    initial commit

---
+++ src/app/service/dataservice.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import * as rx from 'rxjs';
+
+@Injectable()
+export class DataService {
+
+   email: string
+   password: string 
+}
---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import Amplify from 'aws-amplify';
+
+//Amplify.configure(awsconfig); 
+
+//Amplify.configure({
+ // Auth: {
+  //  identityPoolId:'b93897a6-b9d7-45d3-a684-9131b12eb849',
+  //  identityPoolRegion:'us-west-2',
+   // region: 'us-west-2',
+   // userPoolId: 'us-west-2_cQBnJ2oo6',
+   // userPoolWebClientId: '61vjcq4e0ihiphibpekhr6cu14', 
+   // userPoolWebClientSecret:'19actgilihjmcpt09ncn3fdqkaiu2djs5oq8gl3o06m7ehtk2v9k',
+   // mandatorySignIn: true,
+ // } 
+//});
+
+Amplify.configure({
+  Auth: {
+    identityPoolId:'76fa3f59-1088-4529-a9ec-f052cab9a2c1',
+    identityPoolRegion:'us-west-2',
+    region: 'us-west-2',
+    userPoolId: 'us-west-2_rh1Ia1jf4',
+    userPoolWebClientId: '2jieblnjs6pm3ak7f0umrbpsch',
+    mandatorySignIn: true,
+  } 
+});
+
+
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
